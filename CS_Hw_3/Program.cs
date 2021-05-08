df1315a85f3fdf00099ae456c6e5b21e0d9b4831
Mame_ym86@itstep.edu.az <Mame_ym86@itstep.edu.az>
2021-05-08 13:46:47 +0400

    Hw 3 added

---
+++ CS_Hw_3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using MyClassLib.WorldOfTanks;
+
+namespace CS_Hw_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank[] tanks_T_34 = new Tank[5];
+            Tank[] tanks_Pantera = new Tank[5];
+            
+
+
+            // Pre Game
+            for (int i = 0; i < 5; i++)
+            {
+                tanks_T_34[i] = new Tank("T-34");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                tanks_Pantera[i] = new Tank("Pantera");
+            }
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Battle(tanks_T_34[i], tanks_Pantera[i]);
+                Console.WriteLine("Press any key to see the next battle...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+
+        public static void Battle(Tank t1, Tank t2)
+        {
+            Console.WriteLine("=====================================================");
+            Console.WriteLine("\t\t\tT-34\tPantera");
+            Console.WriteLine($"Ammunition Level: \t{t1.AmmunitionLevel}\t{t2.AmmunitionLevel}");
+            Console.WriteLine($"Armor Level: \t\t{t1.ArmorLevel}\t{t2.ArmorLevel}");
+            Console.WriteLine($"Maneuverability Level:  {t1.ManeuverabilityLevel}\t{t2.ManeuverabilityLevel}");
+            Console.WriteLine($"\n\t\tWinner: {t1 * t2}");
+            Console.WriteLine("=====================================================\n\n\n");
+        }
+
+
+    }
+}
---
+++ MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace MyClassLib.WorldOfTanks
+{
+    public class Tank
+    {
+        #region Properties
+
+
+        public string TankName { get; set; }
+
+        public int AmmunitionLevel { get; set; }
+
+        public int ArmorLevel { get; set; }
+
+        public int ManeuverabilityLevel { get; set; }
+
+
+        #endregion
+
+
+        #region Constructer
+
+        public Tank(string tankName)
+        {
+            Random random = new Random();
+            TankName = tankName;
+            System.Threading.Thread.Sleep(50);
+            try
+            {
+                AmmunitionLevel = random.Next(101);
+                ArmorLevel = random.Next(101);
+                ManeuverabilityLevel = random.Next(101);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An exception occured");
+                throw;
+            }
+            
+        }
+
+
+        #endregion
+
+
+        #region Operator Overloading
+
+
+        // 
+        // Takes 2 tanks as parameters
+        // Returns name of the victorious tank or "Draw"
+        public static string operator *(Tank t1, Tank t2)
+        {
+            int t1Score = 0;
+            int t2Score = 0;
+
+            try
+            {
+                if (t1.AmmunitionLevel > t2.AmmunitionLevel)
+                    t1Score++;
+                else if (t1.AmmunitionLevel < t2.AmmunitionLevel)
+                    t2Score++;
+
+                if (t1.ArmorLevel > t2.ArmorLevel)
+                    t1Score++;
+                else if (t1.ArmorLevel < t2.ArmorLevel)
+                    t2Score++;
+
+                if (t1.ManeuverabilityLevel > t2.ManeuverabilityLevel)
+                    t1Score++;
+                else if (t1.ManeuverabilityLevel < t2.ManeuverabilityLevel)
+                    t2Score++;
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occured while battle");
+                throw;
+            }
+
+
+            if (t1Score > t2Score) return t1.TankName;
+            else if (t1Score < t2Score) return t2.TankName;
+
+            return "Draw";
+        }
+
+
+        #endregion
+
+
+
+        public override string ToString()
+        {
+            return $"Name: {TankName}\nAmmunition level: {AmmunitionLevel}\nArmor Level: {ArmorLevel}\nManeuverability Level: {ManeuverabilityLevel}\n";
+        }
+    }
+}
